3cb92b88afa9e2b3829a41e155834077388bd7d3
jselle <jselle@greensystemes.com>
2022-07-21 14:28:09 +0200

    corrected a typo

--- src/entity/enemyExport.js
+++ src/entity/enemyExport.js
@@ -1,3 +1,3 @@
-export {default as Enemy} from "./GroundEnemy.js"
+export {default as Enemy} from "./Enemy.js"
 export {default as GroundEnemy} from "./GroundEnemy.js"
 export {default as FlyingEnemy} from "./FlyingEnemy.js"
